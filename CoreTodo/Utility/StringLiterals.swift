8c6628e35036e3b8a800d9fc9ce722db79c67cd1
yujinkim1 <yujin7621@me.com>
2023-09-14 15:47:28 +0900

    Create StringLiterals.swift

---
+++ CoreTodo/Utility/StringLiterals.swift
@@ -0,0 +1,19 @@
+//
+//  StringLiterals.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-14.
+//
+
+import Foundation
+
+enum StringLiterals {
+    // MARK: IGProfileView
+    static let kPosts: String = "게시"
+    static let kFollowers: String = "팔로워"
+    static let kFollowing: String = "팔로잉"
+    static let kEdit: String = "프로필 편집"
+    static let kShare: String = "프로필 공유"
+    static let kHighlight: String = "스토리 하이라이트"
+    static let kHighlightDescription: String = "좋아하는 스토리를 프로필에 보관해보세요"
+}
