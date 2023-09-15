4a661d422278f944e0f97ccaea46a5e3d6897b6e
yujinkim1 <yujin7621@me.com>
2023-09-15 23:53:17 +0900

    Update MiddleBarHorizontalStackView.swift

--- CoreTodo/Presentation/IGProfileDesignView/Component/MiddleBarHorizontalStackView.swift
+++ CoreTodo/Presentation/IGProfileDesignView/Component/MiddleBarHorizontalStackView.swift
@@ -9,36 +9,32 @@ import SnapKit
 import Then
 import UIKit
 
-final class MiddleBarHorizontalStackView: UIStackView, UIStackViewConfigurable {
-    
-    private lazy var followFillButton: UIButton = .init(type: .custom).then({
-        $0.frame = CGRect(x: 0, y: 0, width: 150, height: 30)
+final class MiddleBarHorizontalStackView: UIStackView {
+    private lazy var followFillButton: UIButton = .init(type: .system).then({
         $0.layer.cornerRadius = CGFloat(8)
         $0.clipsToBounds = true
         $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        $0.setTitle("팔로우", for: .normal)
-        $0.backgroundColor = .systemBlue
+        $0.setTitle(StringLiterals.kFollowFillButton, for: .normal)
+        $0.backgroundColor = UIColor(hex: "#4192EF")
         $0.setTitleColor(UIColor(hex: "#FFFFFF"), for: .normal)
-        $0.setTitleColor(UIColor(hex: "#000000"), for: .highlighted)
+        $0.setTitleColor(UIColor(hex: "#FFFFFF"), for: .highlighted)
     })
     
-    private lazy var messageFillButton: UIButton = .init(type: .custom).then({
-        $0.frame = CGRect(x: 0, y: 0, width: 150, height: 30)
+    private lazy var messageFillButton: UIButton = .init(type: .system).then({
         $0.layer.cornerRadius = CGFloat(8)
         $0.clipsToBounds = true
         $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        $0.setTitle("메시지", for: .normal)
+        $0.setTitle(StringLiterals.kMessageFillButton, for: .normal)
         $0.backgroundColor = .systemGray6
         $0.setTitleColor(UIColor(hex: "#000000"), for: .normal)
         $0.setTitleColor(UIColor(hex: "#000000"), for: .highlighted)
     })
     
-    private lazy var findUserFillButton: UIButton = .init(type: .custom).then({
-        $0.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+    private lazy var findUserFillButton: UIButton = .init(type: .system).then({
         $0.layer.cornerRadius = CGFloat(8)
         $0.clipsToBounds = true
         $0.backgroundColor = .systemGray6
-        $0.setTitle("0", for: .normal)
+        $0.setImage(UIImage(systemName: "person.badge.plus"), for: .normal)
     })
     
     override init(frame: CGRect) {
@@ -50,11 +46,31 @@ final class MiddleBarHorizontalStackView: UIStackView, UIStackViewConfigurable {
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+}
+
+extension MiddleBarHorizontalStackView: UIStackViewConfigurable {
     func configure() {
-        axis = .horizontal
-        spacing = CGFloat(4)
-        alignment = .leading
+        self.axis = .horizontal
+        self.spacing = CGFloat(4)
+        self.alignment = .leading
+        self.backgroundColor = .red
         addArrangedSubviews([followFillButton, messageFillButton, findUserFillButton])
+        setLayout()
+    }
+    
+    func setLayout() {
+        followFillButton.snp.makeConstraints({ constraint in
+            constraint.width.equalTo(150)
+            constraint.bottom.equalToSuperview()
+        })
+        messageFillButton.snp.makeConstraints({ constraint in
+            constraint.width.equalTo(followFillButton.snp.width)
+            constraint.leading.equalTo(followFillButton.snp.trailing).offset(4)
+            constraint.bottom.equalToSuperview()
+        })
+        findUserFillButton.snp.makeConstraints({ constraint in
+            constraint.width.height.equalTo(30)
+            constraint.leading.equalTo(messageFillButton.snp.trailing).offset(4)
+        })
     }
 }
