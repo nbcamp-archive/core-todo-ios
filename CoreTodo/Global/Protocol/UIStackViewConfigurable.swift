db084919b04075dc93ee6a53450f459d9c2f3131
yujinkim1 <yujin7621@me.com>
2023-09-15 17:39:29 +0900

    Create UIStackViewConfigurable.swift

---
+++ CoreTodo/Global/Protocol/UIStackViewConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  UIStackViewConfigurable.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-15.
+//
+
+import UIKit
+
+protocol UIStackViewConfigurable: AnyObject {
+    func configure()
+}
