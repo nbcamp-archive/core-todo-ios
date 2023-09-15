6ab64d5f8c7dcd3182e0f3d35b842ba0a6953a7b
yujinkim1 <yujin7621@me.com>
2023-09-16 00:08:16 +0900

    Create UserInfoVerticalStackView.swift

---
+++ CoreTodo/Presentation/IGProfileDesignView/Component/UserInfoVerticalStackView.swift
@@ -0,0 +1,12 @@
+//
+//  UserInfoVerticalStackView.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-16.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+final class UserInfoVerticalStackView: UIStackView {}
