a9a6836651cb249ccf3b0423ae8758d0fee9e5b6
yujinkim1 <yujin7621@me.com>
2023-09-16 00:01:10 +0900

    Update StringLiterals.swift

--- CoreTodo/Utility/StringLiterals.swift
+++ CoreTodo/Utility/StringLiterals.swift
@@ -8,12 +8,19 @@
 import Foundation
 
 enum StringLiterals {
-    // MARK: IGProfileView
-    static let kPosts: String = "게시"
+    // MARK: - IGProfileView
+    static let kName: String = "묭이"
+    static let kUserName: String = "MyongE_"
+    static let kBio: String = "iOS Developer 🍎"
+    static let kLinkToBio: String = "spartacodingclub.kr"
+    static let kPosts: String = "게시물"
     static let kFollowers: String = "팔로워"
     static let kFollowing: String = "팔로잉"
     static let kEdit: String = "프로필 편집"
     static let kShare: String = "프로필 공유"
     static let kHighlight: String = "스토리 하이라이트"
     static let kHighlightDescription: String = "좋아하는 스토리를 프로필에 보관해보세요"
+    // MARK: - MiddleBarHorizontalStackView
+    static let kFollowFillButton: String = "팔로우"
+    static let kMessageFillButton: String = "메시지"
 }
