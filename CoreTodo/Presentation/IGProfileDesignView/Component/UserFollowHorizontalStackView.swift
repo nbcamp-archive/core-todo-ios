d2c7d282d8c222838ab3feb30effc60819aad7cc
yujinkim1 <yujin7621@me.com>
2023-09-16 00:07:11 +0900

    Create UserFollowHorizontalStackView.swift

---
+++ CoreTodo/Presentation/IGProfileDesignView/Component/UserFollowHorizontalStackView.swift
@@ -0,0 +1,12 @@
+//
+//  UserFollowHorizontalStackView.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-16.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+final class UserFollowHorizontalStackView: UIStackView {}
