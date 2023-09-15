4898e0e336934c926cbe7bfab3a704f2f920e32b
yujinkim1 <yujin7621@me.com>
2023-09-15 14:08:48 +0900

    Create UIViewControllerConfigurable.swift

---
+++ CoreTodo/Global/Protocol/UIViewControllerConfigurable.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerConfigurable.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-15.
+//
+
+import Foundation
+
+protocol UIViewControllerConfigurable {
+    func setUI()
+    func setLayout()
+    func setDelegate()
+    func addTarget()
+}
