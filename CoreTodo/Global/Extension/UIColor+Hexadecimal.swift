325c1f62c1bf1da882de64498cdc70067bd1e135
yujinkim1 <yujin7621@me.com>
2023-09-15 17:37:24 +0900

    Update UIColor+Hexadecimal.swift
    Fix green color object value

--- CoreTodo/Global/Extension/UIColor+Hexadecimal.swift
+++ CoreTodo/Global/Extension/UIColor+Hexadecimal.swift
@@ -20,7 +20,7 @@ extension UIColor {
         Scanner(string: hexadecimalFormat).scanHexInt64(&rgb)
         
         self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgb & 0x0FF0) >> 8) / 255.0,
+                  green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
                   blue: CGFloat(rgb & 0x0000FF) / 255.0,
                   alpha: alpha)
     }
