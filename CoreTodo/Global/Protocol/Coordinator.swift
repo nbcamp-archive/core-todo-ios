1f671291cea64177d4e717b6f5649402e02c8fed
yujinkim1 <yujin7621@me.com>
2023-09-19 11:18:49 +0900

    Update Coordinator.swift
    Add navigation controller

--- CoreTodo/Global/Protocol/Coordinator.swift
+++ CoreTodo/Global/Protocol/Coordinator.swift
@@ -5,9 +5,10 @@
 //  Created by Yujin Kim on 2023-09-14.
 //
 
-import Foundation
+import UIKit
 
 protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get set }
     var childCoordinators: [Coordinator] { get set }
     func start()
 }
