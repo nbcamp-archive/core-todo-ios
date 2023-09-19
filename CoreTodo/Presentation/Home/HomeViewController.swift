a9de191a456259f8a2f898f1c1c07473fcfc36d7
yujinkim1 <yujin7621@me.com>
2023-09-19 12:26:00 +0900

    Update HomeViewController.swift

--- CoreTodo/Presentation/Home/HomeViewController.swift
+++ CoreTodo/Presentation/Home/HomeViewController.swift
@@ -5,4 +5,39 @@
 //  Created by Yujin Kim on 2023-09-19.
 //
 
-import Foundation
+import SnapKit
+import Then
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private lazy var homeViewLabel: UILabel = .init().then({
+        $0.text = "홈 화면"
+        $0.font = UIFont.systemFont(ofSize: 22)
+    })
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension HomeViewController: UIViewControllerConfigurable {
+    
+    func setUI() {
+        view.addSubview(homeViewLabel)
+    }
+    
+    func setLayout() {
+        homeViewLabel.snp.makeConstraints({ constraint in
+            constraint.centerX.equalToSuperview()
+            constraint.centerY.equalToSuperview()
+        })
+    }
+    
+    func setDelegate() {}
+    
+    func addTarget() {}
+    
+}
