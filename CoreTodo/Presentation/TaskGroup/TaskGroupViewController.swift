2771137668c9f440f214a540e58f81cfcda5ddc9
yujinkim1 <yujin7621@me.com>
2023-09-20 17:57:07 +0900

    Create TaskGroupViewController.swift

---
+++ CoreTodo/Presentation/TaskGroup/TaskGroupViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TaskGroupViewController.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-20.
+//
+
+import UIKit
+
+class TaskGroupViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
