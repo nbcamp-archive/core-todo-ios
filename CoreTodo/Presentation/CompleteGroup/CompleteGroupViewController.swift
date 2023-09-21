370c8ec80e5cc4931748fe5d37c745eeb0488896
yujinkim1 <yujin7621@me.com>
2023-09-21 21:13:36 +0900

    Create CompleteGroup

---
+++ CoreTodo/Presentation/CompleteGroup/CompleteGroupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CompleteGroupViewController.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-21.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+class CompleteGroupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ CoreTodo/Presentation/CompleteGroup/CompleteGroupViewCoordinator.swift
@@ -0,0 +1,10 @@
+//
+//  CompleteGroupViewCoordinator.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-21.
+//
+
+import UIKit
+
+class CompleteGroupViewCoordinator {}
