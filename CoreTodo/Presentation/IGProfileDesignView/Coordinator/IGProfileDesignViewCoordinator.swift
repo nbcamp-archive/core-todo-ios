e89a3ac10a3bce82067413c79ccf875b0be45eab
yujinkim1 <yujin7621@me.com>
2023-09-21 20:01:24 +0900

    Create IGProfileDesignViewCoordinator.swift

---
+++ CoreTodo/Presentation/IGProfileDesignView/Coordinator/IGProfileDesignViewCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  IGProfileDesignViewCoordinator.swift
+//  CoreTodo
+//
+//  Created by Yujin Kim on 2023-09-21.
+//
+
+import UIKit
+
+class IGProfileDesignViewCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    var childCoordinators: [Coordinator]
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.childCoordinators = [Coordinator]()
+    }
+    
+    func start() {
+        let viewController = IGProfileDesignViewController()
+        viewController.coordinator = self
+        
+        navigationController.isNavigationBarHidden = false
+        navigationController.navigationBar.prefersLargeTitles = false
+        navigationController.navigationBar.tintColor = .black
+        navigationController.pushViewController(viewController, animated: false)
+    }
+
+}
